1d8ded76b1b48df343fd239761e27bdddfb9e220
vivek <vivekvikky169@gmail.com>
2020-04-27 17:17:41 +0530

    removed retelimitter

--- thandra/route.js
+++ thandra/route.js
@@ -3,12 +3,12 @@ const carRoute = require('./routes/car')
 const userRoute = require('./routes/user')
 const userRequestRoute = require('./routes/userRequest')
 const adminRoute = require('./routes/admin')
-const rateLimiterRedisMiddleware = require('./middleware/rateLimiterRedis');
+// const rateLimiterRedisMiddleware = require('./middleware/rateLimiterRedis');
 
 
 module.exports = function(app) {
     app.use(express.json());
-    app.use(rateLimiterRedisMiddleware);
+    // app.use(rateLimiterRedisMiddleware);
     app.use("/car", carRoute);
     app.use("/user", userRoute);
     app.use("/userReq", userRequestRoute);
